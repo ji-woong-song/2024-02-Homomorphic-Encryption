8ca37957ad0cfaf8bdea1bf9034ba18d5142591d
송지웅 <thdwldnd7@dgu.ac.kr>
2024-12-14 02:21:23 +0900

    feat: calculator 추가

---
+++ domain/calculator/calculator.cpp
@@ -0,0 +1,33 @@
+//
+// Created by jiwoongDev on 2024-12-14.
+//
+
+#include "calculator.h"
+
+size_t Calculator::get_numerator_reverse_element(size_t x) {
+    size_t numerator = get_numerator(x);
+    size_t c = 0;
+    for (size_t i = 1; i < plain_modulus; i++) {
+        if ((numerator * i) % plain_modulus == 1) {
+            c = i;
+            break;
+        }
+    }
+    std::cout << "numerator : " << numerator << "c : " << c << "(numerator * c) % plain_modulus ";
+    std::cout << (numerator * c) % plain_modulus <<  "\n";
+    return c;
+}
+
+size_t Calculator::get_numerator(const size_t& target) {
+    size_t numerator = 1;
+    for (int i = 0; i <= width; i++) {
+        if (i == target) continue;
+        if (i < target) {
+            numerator *= (target - i);
+        } else if (i > target) {
+            numerator *= (i - target);
+        }
+    }
+    std::cout << "numerator : " << numerator << "\n";
+    return numerator;
+}
---
+++ domain/calculator/calculator.h
@@ -0,0 +1,26 @@
+//
+// Created by jiwoongDev on 2024-12-14.
+//
+
+#ifndef SEAL_REAL_CALCULATOR_H
+#define SEAL_REAL_CALCULATOR_H
+
+#include "seal/seal.h"
+#include <vector>
+
+class Calculator {
+private:
+    seal::SEALContext& context;
+    size_t plain_modulus;
+    int width;
+public:
+    Calculator(seal::SEALContext& context, int width) : context(context), width(width) {
+        this->plain_modulus = context.first_context_data()->parms().plain_modulus().value();
+    }
+
+    size_t get_numerator_reverse_element(size_t x);
+    size_t get_numerator(const size_t& target);
+};
+
+
+#endif //SEAL_REAL_CALCULATOR_H
